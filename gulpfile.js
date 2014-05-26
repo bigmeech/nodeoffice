9893ad1397777227e5873e78947951ba72e675df
bigmeech <denachural@gmail.com>
2014-05-26 23:55:11 +0100

    Added Gulp file for automated builds

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require("gulp");
+
+gulp.task("default",function(){
+  console.log("this is my first task!!!")
+});
+
+gulp.task("build-component",function(){
+  console.log("this should run component CLI!!!")
+})
