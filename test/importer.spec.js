59320c47bf818fde01c49eba4fcebfa18087e2a1
larry eliemenye <larry.eliemenye@massive.co>
2018-03-20 16:40:31 +0000

    new file and api structure

--- test/importer.spec.js
+++ test/importer.spec.js
@@ -1,8 +1,43 @@
 const { expect } = require('chai');
-const NodeOffice = require('../lib/index');
+const Importer = require('../lib/importer');
 describe('importer', function() {
     it('should throw an error for an unrecognised file type', () => {
         const someFileType = './path/to/docs.mov';
-        expect(NodeOffice.Importer.bind(null, someFileType)).to.throw();
-    })
+        expect(Importer.bind(null, someFileType)).to.throw();
+    });
+
+    it('should not throw for a docx file', () => {
+        const someFileType = 'sample.docx';
+        expect(Importer.bind(null, someFileType)).to.not.throw();
+    });
+
+    it('should return an array of paragraphs', async () => {
+        const someFileType = 'sample.docx';
+        const word = await Importer(someFileType);
+        expect(word.getParagraphs()).to.be.an('array');
+    });
+
+    it('should return an array of paragraph text', async () => {
+        const someFileType = 'sample.docx';
+        const word = await Importer(someFileType);
+        expect(word.getText()).to.be.an('array');
+    });
+
+    it('should return an array of paragraph ids', async () => {
+        const someFileType = 'sample.docx';
+        const word = await Importer(someFileType);
+        expect(word.getParagraphIds()).to.be.an('array');
+    });
+
+    it('should return a paragraph by id', async () => {
+        const someFileType = 'sample.docx';
+        const word = await Importer(someFileType);
+        expect(word.getParagraphIds()).to.be.an('array');
+    });
+
+    it('should return a text by paragraph id', async () => {
+        const someFileType = 'sample.docx';
+        const word = await Importer(someFileType);
+        expect(word.getTextByParaId('53547906')).to.be.equal('What happened');
+    });
 });
